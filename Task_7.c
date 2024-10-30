004a2f51cb3a18da6533b4bcccb202bd2e503bdf
Huxaifa-Nadeem <huzaifanadeeem8@gmail.com>
2024-10-30 23:12:47 +0500

    Add files via upload

---
+++ Task_7.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+    char grid[105][105] = {};
+    int i = 4, j = 0;
+    int up = 1;
+    int jump = 1;
+    int count = 0;
+
+    printf("Calibrating: \n");
+    while (count != 3)
+    {
+        printf("%d %d %d\n", i, j, jump);
+        grid[i][j] = '*';
+
+        if (up)
+        {
+            i--;
+            j += jump;
+            jump++;
+        }
+        else
+        {
+            i++;
+            j += jump;
+            jump--;
+        }
+        if (i == 0 && up == 1)
+        {
+            up = 0;
+            jump--;
+        }
+        else if (i == 4 && up == 0)
+        {
+            jump = 1;
+            up = 1;
+            count++;
+            if (count != 0)
+            {
+                grid[i][j] = '*';
+                j += 2;
+                if (count != 3)
+                    grid[i][j] = '*';
+            }
+        }
+    }
+
+    printf("Pattern:\n");
+    int i2 = 0;
+    while (i2 <= 4)
+    {
+        int j2 = 0;
+        while (j2 < 105)
+        {
+            if (grid[i2][j2] == '*')
+            {
+                printf("%c", grid[i2][j2]);
+            }
+            else
+            {
+                printf(" ");
+            }
+            j2++;
+        }
+        printf("\n");
+        i2++;
+    }
+}
